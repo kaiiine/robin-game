0434c87bdff9ae85c8837dd9169bbc01563afbd1
Nassim <nassim.jazouli@epfedu.fr>
2026-02-24 12:03:26 +0100

    fix : files paths problem

--- src/sound.py
+++ src/sound.py
@@ -1,13 +1,16 @@
 import pygame
+from pathlib import Path
+
 class Sound():
 
 
     def __init__(self):
-        self.music=pygame.mixer.music.load("../sound/level_1_sound.mp3")
+        sound_dir = Path(__file__).parent.parent / "sound"
+        self.music=pygame.mixer.music.load(str(sound_dir / "level_1_sound.mp3"))
         self.music_volume=pygame.mixer.music.set_volume(0.25)
         self.music_play=pygame.mixer.music.play(loops=1)
-        self.mario_jump=pygame.mixer.Sound("../sound/mario_jump.mp3")
-        self.mario_death=pygame.mixer.Sound("../sound/mario_death.mp3")
+        self.mario_jump=pygame.mixer.Sound(str(sound_dir / "mario_jump.mp3"))
+        self.mario_death=pygame.mixer.Sound(str(sound_dir / "mario_death.mp3"))
         self.sound_volume()
 
 
